ac5d246e7685528143fcac309277cf117cccbaeb
Ramakrishna <ramakrsnabhat@gmail.com>
2026-01-13 20:27:48 +0530

    Fix: Vercel 404 Routing request

--- server/src/index.ts
+++ server/src/index.ts
@@ -52,6 +52,10 @@ app.get('/', (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+export default app;
